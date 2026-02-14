c36bf571589b2f70105d5adc3a4e75e8550f53a8
Abhimanyu Tiwari <theabhimanyutiwari@gmail.com>
2026-02-14 21:28:09 +0530

    feat: add mongodb connection

--- 02-database-integration/server.js
+++ 02-database-integration/server.js
@@ -1,4 +1,15 @@
+require("dotenv").config();
 const app = require("./src/app");
+const mongoose = require("mongoose");
+
+function ConnecttoDB(){
+    mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log("Connected Sucessfully")
+    })
+}
+
+ConnecttoDB()
 app.listen(3000,(req,res)=>{
     console.log("server is running on port 3000")
 })
