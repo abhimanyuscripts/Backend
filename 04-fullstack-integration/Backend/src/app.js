13c08f3a2fdb24ac26996e0bcb7bf9735bebf2e5
Abhimanyu Tiwari <theabhimanyutiwari@gmail.com>
2026-09-17 11:17:34 +0530

    worked on complete backend

--- 04-fullstack-integration/Backend/src/app.js
+++ 04-fullstack-integration/Backend/src/app.js
@@ -1,52 +1,20 @@
 const express = require("express")
-const noteModel = require("./model/notes.model")
-
 const app = express()
 app.use(express.json())
+const noteModel = require("./models/note.model")
 
-app.post("/api/notes", async (req,res)=>{
-    const {title,description} = req.body
+app.post('/api/notes',async (req,res)=>{
+    const {title , description} = req.body;
 
     const note = await noteModel.create({
         title,description
     })
-
     res.status(201).json({
-        message : "note created ",
+        message : "Note Created Successfully",
         note
     })
 })
 
-app.get("/api/notes",async (req,res)=>{
 
-    const notes = await noteModel.find()
-
-    res.status(200).json({
-        message: " fetched",
-        notes
-    })
-
-})
-
-app.delete("/api/notes/:id",async(req,res)=>{
-    const id = req.params.id
-
-    await noteModel.findByIdAndDelete(id)
-
-    res.status(200).json({
-        message:"note deleted seccessfully",
-    })
-})
-
-app.patch("/api/notes/:id",async (req,res)=>{
-    const id = req.params.id
-    const {description} = req.body
-
-    await noteModel.findByIdAndUpdate(id, { description })
-
-    res.status(200).json({
-        message:"note updated successfully."
-    })
-})
 
 module.exports = app
--- 04-fullstack-integration/Backend/src/config/database.js
+++ 04-fullstack-integration/Backend/src/config/database.js
@@ -1,10 +1,12 @@
-const mongoose = require("mongoose")
+require("dotenv").config()
 
+const mongoose = require('mongoose');
 function connectToDB(){
-    mongoose.connect(process.env.MONGO_URI).then(()=>
-        console.log("connected to db")
-    )
+    mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log("Connected to MongoDB")
+    })
+    
 }
-
 
 module.exports = connectToDB
